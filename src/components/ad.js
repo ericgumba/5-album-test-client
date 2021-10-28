fe71360b9cd67efc3923adc6a9472f2a3e1a3f16
ericgumba <ericgumba@gmail.com>
2021-10-28 16:54:30 -0700

    using adsense library

--- src/components/ad.js
+++ src/components/ad.js
@@ -3,23 +3,26 @@
 import React from 'react';
 // import AdSense
 // import AdSense
-import AdSense from 'react-ssr-adsense';
+import AdSense from 'react-adsense';
 
 export default function AdComponent (){ 
- 
-    React.useEffect(() => {
-        window.adsbygoogle = window.adsbygoogle || [];
-        window.adsbygoogle.push({});
-    }, [])
+  
  
     return (
         <div>
-            <ins class="adsbygoogle"
+            {/* <ins class="adsbygoogle"
             style={{display:"fluid"}}
             data-ad-client="ca-pub-9137217174806568"
             data-ad-slot="8721475502"
             data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
+            data-full-width-responsive="true"></ins> */} 
+            <AdSense.Google
+            client='ca-pub-9137217174806568'
+            slot='8721475502'
+            style={{ display: 'block' }}
+            layout='in-article'
+            format='fluid'
+            />
         </div>
     ); 
 } 
